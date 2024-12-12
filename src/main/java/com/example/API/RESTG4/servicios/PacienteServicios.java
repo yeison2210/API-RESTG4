7bf4602ebeadcd7b921959935b6d1d237b0a9a08
heidyquiceno2006 <heidyquiceno2@gmail.com>
2024-12-12 18:37:27 -0500

    se crean los servicios

--- src/main/java/com/example/API/RESTG4/servicios/PacienteServicios.java
+++ src/main/java/com/example/API/RESTG4/servicios/PacienteServicios.java
@@ -1,4 +1,37 @@
 package com.example.API.RESTG4.servicios;
 
+import com.example.API.RESTG4.modelos.Paciente;
+import com.example.API.RESTG4.repositorios.IRepositorioPaciente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PacienteServicios {
+    @Autowired
+    IRepositorioPaciente iRepositorioPaciente;
+
+    //listo las operaciones que voy a realizar en base de datos
+
+    //registar un paciente
+    public Paciente registarPaciente(Paciente datosPaciente) throws Exception{
+
+        try {
+
+            //guardar en la base de datos del paciente
+            return iRepositorioPaciente.save(datosPaciente);
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    //buscar los pacientes
+    public List<Paciente> buscarPacientes() throws Exception{
+        try {
+            return iRepositorioPaciente.findAll();
+        }catch (Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
 }
