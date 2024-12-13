b534fcb2e2eed506d7a21b6bb24200cfd7736246
anacano <ana.cano3012@gmail.com>
2024-12-13 12:07:40 -0500

    se crean controladores

--- src/main/java/com/example/API/RESTG4/controladores/PacienteControlador.java
+++ src/main/java/com/example/API/RESTG4/controladores/PacienteControlador.java
@@ -1,4 +1,44 @@
 package com.example.API.RESTG4.controladores;
 
+import com.example.API.RESTG4.modelos.Paciente;
+import com.example.API.RESTG4.servicios.PacienteServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/paciente")
 public class PacienteControlador {
+
+    @Autowired
+    PacienteServicio pacienteServicio;
+
+    @PostMapping
+    public ResponseEntity<?> guardar(@RequestBody Paciente datos){
+        try {
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(pacienteServicio.registarPaciente(datos));
+        }catch (Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> buscarTodo(){
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(pacienteServicio.buscarPacientes());
+
+        }catch (Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+        }
+    }
 }
--- src/main/java/com/example/API/RESTG4/controladores/SignoVitalControlador.java
+++ src/main/java/com/example/API/RESTG4/controladores/SignoVitalControlador.java
@@ -1,4 +1,46 @@
 package com.example.API.RESTG4.controladores;
 
+import com.example.API.RESTG4.modelos.Paciente;
+import com.example.API.RESTG4.modelos.SignoVital;
+import com.example.API.RESTG4.servicios.PacienteServicio;
+import com.example.API.RESTG4.servicios.SignoVitalServicios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/signovital")
+
 public class SignoVitalControlador {
+
+    @Autowired
+    SignoVitalServicios signoVitalServicios;
+
+    @PostMapping
+    public ResponseEntity<?> guardar(@RequestBody SignoVital datos){
+        try {
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(signoVitalServicios.registroSignoVital(datos));
+        }catch (Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> buscarTodo(){
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(signoVitalServicios.buscarSignoVital());
+        }catch (Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+        }
+    }
 }
