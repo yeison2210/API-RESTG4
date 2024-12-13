67933911a49443f54c6aa33f3ae1fd2d362478b3
yeison2210 <yeison221020@gmail.com>
2024-12-13 11:29:54 -0500

    Ejemplo controlador

--- src/main/java/com/example/API/RESTG4/controladores/EnfermedadControlador.java
+++ src/main/java/com/example/API/RESTG4/controladores/EnfermedadControlador.java
@@ -1,4 +1,46 @@
 package com.example.API.RESTG4.controladores;
 
+import com.example.API.RESTG4.modelos.Enfermedad;
+import com.example.API.RESTG4.modelos.Paciente;
+import com.example.API.RESTG4.servicios.EnfermedadServicios;
+import com.example.API.RESTG4.servicios.PacienteServicios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/enfermedad")
 public class EnfermedadControlador {
+
+
+    @Autowired
+    EnfermedadServicios enfermedadServicio;
+
+    @PostMapping
+    public ResponseEntity<?> guardar(@RequestBody Enfermedad datos){
+        try {
+
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(enfermedadServicio.registroEnfermedad(datos));
+        }catch (Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<?> buscarTodo(){
+        try {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(enfermedadServicio.buscarEnfermedad());
+        }catch (Exception error){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(error.getMessage());
+        }
+    }
 }
